9727f0f55c84039fb031b654d7b4532a480eeb0e
Maria Gabriela <mariagabriela236@gmail.com>
2022-04-16 17:30:42 -0400

    first endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController //makes class to serve REST endpoints
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping // Get-Post...
+	public String hello(){
+		return "Hewwo World";
 	}
 
 }
